2c0979f553e551c7763b1bc92f2b876f187330d4
SantiCastelli <castellist@gmail.com>
2022-03-08 13:54:53 +0100

    terminado

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
 // Implementar solución aquí
 
+let cambiarBody = document.querySelector("#btn");
+
+
+cambiarBody.addEventListener("click", function () {
+    console.log("estoy aca");
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.querySelector("body").style.backgroundColor = "#" + randomColor;
+    document.querySelector("#hex-value").textContent =   "#" + randomColor;
+})
